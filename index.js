aeea6dd06c2d5acec05f7d0f762a599604bf007c
samuel <samuelibiapinolima@gmail.com>
2020-05-20 14:45:14 -0300

    rotas funcionando

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const path = require('path');
 const config = require('./config/config');
+const apiRoutes = require('./routes/api');
 
 const app = express();
 
+app.use('/api', apiRoutes);
 app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.listen(config.PORT, () => {console.log('Server running on port:', config.PORT)});
--- routes/api.js
+++ routes/api.js
@@ -1,1 +1,8 @@
 const express = require('express');
+const router = express.Router();
+
+router.get('/clientes', (req, res) => {
+  res.send('Tudo certo por aqui!');
+});
+
+module.exports = router;
