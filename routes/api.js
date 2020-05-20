6962dbb64f2c508075ed0fbf0ccdf3a4055cb524
samuel <samuelibiapinolima@gmail.com>
2020-05-20 14:54:37 -0300

    get e post funcionando

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const clientes = require('../model/data/clientes');
 const router = express.Router();
 
 router.get('/clientes', (req, res) => {
-  res.send('Tudo certo por aqui!');
+  res.json(JSON.stringify(clientes.getAllClients()));
 });
+
+router.post('/novo', bodyParser.json(), (req, res) => {
+  let name = req.body.name;
+  let description = req.body.description;
+
+  clientes.newClient(name, description);
+  res.send({status: 200, msg: 'Cliente cadastrado com sucesso!'});
+})
 
 module.exports = router;
